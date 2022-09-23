b9dd9d4ba0099c6eb30743debf2519a946414ec8
@mulhem.hamdan <mulhem.hamdan@gmail.com>
2022-09-23 15:00:02 +0300

    error handling in update pranch

--- public/js/JQfunction.js
+++ public/js/JQfunction.js
@@ -4,6 +4,11 @@ var isShowData = false;//if the showdata button is clicked
 var updateDelet = [];//the array of updated and deleted data from old data that have been shown 
 var rowID = 0;//the container that catch row id when edit button is clicked for any row has been shown 
 var isEdit = false;//if the button edit is clicked on any row in the table
+var update = [];//for stor updated data befor turn it to jason.
+var _id = 0;
+var _name = "";
+var _location = "";
+var _geolocation = "";
 //Edit button
 function BindData(count) {
   $("#name").val($("#name_" + count).html());
@@ -14,37 +19,89 @@ function BindData(count) {
   $("#add").html($("#up").html());
   $("#add").attr("onclick", "EditData(" + count + ")");
   if(isShowData){
+    _name = $("#name").val();
+    _location = $("#location").val();
+    _geolocation = $("#geolocation").val();
     var selector = "#id_"+ count;
     console.log(selector);
-    rowID = $(selector).html();
-    console.log(rowID);
-    isEdit = true;
+    _id = $(selector).html();
+    console.log(_id);
+    //isEdit = true;
   }
 }
 //update button
 function EditData(count) {
-  if(isShowData){
-    $("#add").prop('disabled',true);
-    //make apdate jason from update array
+  if($("#name").val() == ""){
+    $("#nameReq").removeClass("dis-none");
+    $("#name").focus();
+  }
+  else if($("#location").val() == ""){
+    $("#nameReq").addClass("dis-none");
+    $("#locationReq").removeClass("dis-none");
+    $("#location").focus();
+  }
+  else if($("#geolocation").val() == ""){
+    $("#nameReq").addClass("dis-none");
+    $("#locationReq").addClass("dis-none");
+    $("#geolocationReq").removeClass("dis-none");
+    $("#geolocation").focus();
+  }
+  else{
+    $("#nameReq").addClass("dis-none");
+    $("#geolocationReq").addClass("dis-none");
+    $("#locationReq").addClass("dis-none");
+    if(isShowData){
+      $("#add").prop('disabled',true);
+      //make apdate jason from update array
+      /*
+      if(update.length != 0){
+        obj = {};
+        for(i = 0; i < update.length; i =i + 2){
+          obj[update[i]] = update[i + 1];
+        }
+        updateDelet.push(obj);
+        console.log(updateDelet);
+      }
+      */
+     if(_name !== $("#name").val() || _location !== $("#location").val() || _geolocation !== $("#geolocation").val()){
+      update.push("id");
+      update.push(_id);
+     }
+     if(_name !== $("#name").val()){
+       update.push("name");
+       update.push($("#name").val());
+     }
+     if(_location !== $("#location").val()){
+      update.push("location");
+      update.push($("#location").val());
+    }
+    if(_geolocation !== $("#geolocation").val()){
+      update.push("geolocation");
+      update.push($("#geolocation").val());
+    }
+    console.log(update);
     if(update.length != 0){
       obj = {};
       for(i = 0; i < update.length; i =i + 2){
         obj[update[i]] = update[i + 1];
       }
+      console.log(obj);
       updateDelet.push(obj);
       console.log(updateDelet);
+      update = [];
     }
-  }
-  $("#name_" + count).html($("#name").val());
-  $("#location_" + count).html($("#location").val());
-  $("#geolocation_" + count).html($("#geolocation").val());
-  $("#add").attr("value", "add");
-  $("#add").html($("#ad").html());
-  $("#add").attr("onclick", "AddData()");
-  //AddData(); /// ADD NEW DATA
-  $("#name").val("");
-  $("#location").val("");
-  $("#geolocation").val("");
+    }
+    $("#name_" + count).html($("#name").val());
+    $("#location_" + count).html($("#location").val());
+    $("#geolocation_" + count).html($("#geolocation").val());
+    $("#add").attr("value", "add");
+    $("#add").html($("#ad").html());
+    $("#add").attr("onclick", "AddData()");
+    //AddData(); /// ADD NEW DATA
+    $("#name").val("");
+    $("#location").val("");
+    $("#geolocation").val("");
+}
 }
 
 function AddData() {
@@ -56,13 +113,14 @@ function AddData() {
     $("#nameReq").removeClass("dis-none");
     $("#name").focus();
   }else{
-          $("#nameReq").addClass("dis-none");
           if(pranch_location == ""){
+            $("#nameReq").addClass("dis-none");
             $("#locationReq").removeClass("dis-none");
             $("#location").focus();
           }else{
-                  $("#locationReq").addClass("dis-none");
                   if(pranch_geolocation == ""){
+                    $("#locationReq").addClass("dis-none");
+                    $("#nameReq").addClass("dis-none");
                     $("#geolocationReq").removeClass("dis-none");
                     $("#geolocation").focus();
                   }
@@ -70,6 +128,8 @@ function AddData() {
                   else
                   {
                     $("#geolocationReq").addClass("dis-none");
+                    $("#locationReq").addClass("dis-none");
+                    $("#nameReq").addClass("dis-none");
                     $(".tablbody").append('<tr><td id="id_' + count + '">' + count + '</td><td id="name_' + count + '">' + pranch_name + '</td><td id="location_' +
                     count + '">' + pranch_location +
                     '</td><td id="geolocation_' + count + '">' + pranch_geolocation +'</td><td><button type="button" class="delete btn btn-primary">Delete</button></td><td><button type="button" id="edit" onclick="BindData('+ count +');" class="btn btn-primary">Edit</button></td>');
@@ -94,7 +154,7 @@ function subTable(){
     $.ajax({
       type: "POST",
       url: $("#delEdiUrl").html(),
-      data: "pTableData="+TableData+"&_token="+$("input[name=_token]").attr("value"),
+      data: "pTableData="+ updateDelet +"&_token="+$("input[name=_token]").attr("value"),
       success: function(msg){
           // return value stored in msg variable
           //maby you want to add the (clear code) here after you sure that data is recived to server
@@ -120,7 +180,7 @@ function subTable(){
 
       $('.table tbody tr').each(function(row, tr){
           TableData[row]={
-              "Number" : $(tr).find('td:eq(0)').text()
+              "id" : $(tr).find('td:eq(0)').text()
               , "name" :$(tr).find('td:eq(1)').text()
               , "location" : $(tr).find('td:eq(2)').text()
               , "geolocation" : $(tr).find('td:eq(3)').text()
@@ -183,6 +243,8 @@ if(cnt==0){
 }
 }
 //------------------------------------------------------------
+//............................................................
+/*
 var update = [];
 function Change(id){
     if(isEdit){
@@ -195,7 +257,8 @@ function Change(id){
     console.log(update);
     }
   }
-
+  */
+//............................................................
 //------------------------------------------------------------
 $(document).ready(function() {
   $("#add").prop('disabled',false);
@@ -204,8 +267,8 @@ $(document).ready(function() {
   $(document).on('click', '.delete', function() {
     var par = $(this).parent().parent(); //tr
     if(isShowData){
-      var temp = par.find('td:eq(0)').text();
-      updateDelet.push({id:temp});
+      //var temp = par.find('td:eq(0)').text();
+      updateDelet.push({id:par.find('td:eq(0)').text()});
       console.log(updateDelet);
       par.css("background-color","red");
       $(this).prop('disabled',true);
