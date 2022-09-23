3fe4c192b351488a320c3d4ada3af419b6160771
@mulhem.hamdan <mulhem.hamdan@gmail.com>
2022-09-24 00:10:05 +0300

    conflict correctness

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -39,57 +39,6 @@ class BranchController extends Controller
 
   public function store(Request $request)
   {
-<<<<<<< HEAD
-    $i = 0;
-    $arr = new ToArray();
-    $datas = json_decode($request->pTableData, true);
-    $company = session('company');
-    $branches = $arr->to_array(Branch::query()->select('name')->where('company_id', $company)->get(), "name");
-    foreach($datas as $data){
-      isset($data['geolocation'])?$data['geolocation'] = explode(",", $data['geolocation']):[null];
-      $data['latitude'] = $data['geolocation'][0];
-      $data['longitude'] = $data['geolocation'][1];
-      $validated = Validator::make($data,
-      [
-        'name' => ['required', 'string', 'max:50', Rule::notIn($branches),],
-        'location' => ['required', 'string', 'max:50'],
-        'latitude' => [ 'numeric', 'between:-90,90'],
-        'longitude' => [ 'numeric', 'between:-180,180'],
-      ]);
-      if ($validated->fails()) {
-        $status[$i] = $validated->errors();
-        $i++;
-        $error = true;
-        continue;
-      }
-      try {
-        $data_arr =array_merge($data,['company_id' => $company]);
-        $branch = Branch::create($data_arr);
-        $status[$i] = $branch;
-        $i++;
-      }catch (\Exception $e) {
-        return response()->json(new Message($e->getMessage(), '100', false, 'error', 'error', 'خطأ'));
-      }
-    }
-    return response()->json(new Message($status, '200', isset($error)?false:true, 'info', "here status of every insertion", 'هذه حالة كل عملية إدخال'));
-  }
-
-  public function del_edi(Request $request)
-  {
-    $i = 0;
-    $arr = new ToArray();
-    $data = json_decode($request->pTableData, true);
-    $company = session('company');
-    $branches = $arr->to_array(Branch::query()->select('name')->where('company_id', $company)->get(), "name");
-    foreach($data as $data){
-      if($data['geolocation']){
-        $data['geolocation'] = explode(",", $data['geolocation']);
-        $data['latitude'] = $data['geolocation'][0];
-        $data['longitude'] = $data['geolocation'][1];
-      }
-      if(count($data) > 1)
-      {
-=======
     try {
       $i = 0;
       $arr = new ToArray();
@@ -103,7 +52,6 @@ class BranchController extends Controller
           $data['latitude'] = $data['geolocation'][0];
           $data['longitude'] = $data['geolocation'][1];/////make this value nullable in database
         }
->>>>>>> d7bc21bd47423b2e6493c2b6072209cee26aae00
         $validated = Validator::make($data,
         [
           'name' => ['required', 'string', 'max:50', Rule::notIn($branches),],
