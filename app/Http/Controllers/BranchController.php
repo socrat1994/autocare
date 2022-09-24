9d5486baf3832b84b5a1847fa7f1be85b26b6284
socrat <skratgh@gmail.com>
2022-09-24 10:04:06 +0300

    fixing conflict

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -39,101 +39,107 @@ class BranchController extends Controller
 
   public function store(Request $request)
   {
-    $i = 0;
-    $arr = new ToArray();
-    $datas = json_decode($request->pTableData, true);
-    $company = session('company');
-    $branches = $arr->to_array(Branch::query()->select('name')->where('company_id', $company)->get(), "name");
-    foreach($datas as $data){
-      isset($data['geolocation'])?$data['geolocation'] = explode(",", $data['geolocation']):[null];
-      $data['latitude'] = $data['geolocation'][0];
-      $data['longitude'] = $data['geolocation'][1];
-      $validated = Validator::make($data,
-      [
-        'name' => ['required', 'string', 'max:50', Rule::notIn($branches),],
-        'location' => ['required', 'string', 'max:50'],
-        'latitude' => [ 'numeric', 'between:-90,90'],
-        'longitude' => [ 'numeric', 'between:-180,180'],
-      ]);
-      if ($validated->fails()) {
-        $status[$i] = $validated->errors();
-        $i++;
-        $error = true;
-        continue;
-      }
-      try {
-        $data_arr =array_merge($data,['company_id' => $company]);
-        $branch = Branch::create($data_arr);
-        $status[$i] = $branch;
-        $i++;
-      }catch (\Exception $e) {
-        return response()->json(new Message($e->getMessage(), '100', false, 'error', 'error', 'خطأ'));
-      }
-    }
-    return response()->json(new Message($status, '200', isset($error)?false:true, 'info', "here status of every insertion", 'هذه حالة كل عملية إدخال'));
-  }
-
-  public function del_edi(Request $request)
-  {
-    $i = 0;
-    $arr = new ToArray();
-    $data = json_decode($request->pTableData, true);
-    $company = session('company');
-    $branches = $arr->to_array(Branch::query()->select('name')->where('company_id', $company)->get(), "name");
-    foreach($data as $data){
-      if($data['geolocation']){
-        $data['geolocation'] = explode(",", $data['geolocation']);
-        $data['latitude'] = $data['geolocation'][0];
-        $data['longitude'] = $data['geolocation'][1];
-      }
-      if(count($data) > 1)
-      {
+    try {
+      $i = 0;
+      $arr = new ToArray();
+      $datas = json_decode($request->pTableData, true);
+      $company = session('company');
+      $branches = $arr->to_array(Branch::query()->select('name')->where('company_id', $company)->get(), "name");
+      foreach($datas as $data){
+        if($data['geolocation']??null)
+        {
+          $data['geolocation'] = explode(",", $data['geolocation']);
+          $data['latitude'] = $data['geolocation'][0];
+          $data['longitude'] = $data['geolocation'][1];/////make this value nullable in database
+        }
         $validated = Validator::make($data,
-        ['id' => ['required','integer'],
-        'name' => ['string', 'max:50',Rule::notIn($branches) ],
-        'location' => ['string', 'max:50'],
-        'latitude' => [ 'numeric', 'between:-90,90'],
-        'longitude' => [ 'numeric', 'between:-180,180']]);
+        [
+          'name' => ['required', 'string', 'max:50', Rule::notIn($branches),],
+          'location' => ['required', 'string', 'max:50'],
+          'latitude' => [ 'numeric', 'between:-90,90'],
+          'longitude' => [ 'numeric', 'between:-180,180'],
+        ]);
         if ($validated->fails()) {
           $status[$i] = $validated->errors();
           $i++;
           $error = true;
           continue;
         }
+
+        $data_arr =array_merge($data,['company_id' => $company]);
+        $branch = Branch::create($data_arr);
+        $status[$i] = $branch;
+        $i++;
+
+      }  }catch (\Exception $e) {
+        return response()->json(new Message($e->getMessage(), '100', false, 'error', 'error', 'خطأ'));
       }
+      return response()->json(new Message($status, '200', isset($error)?false:true, 'info', "here status of every insertion", 'هذه حالة كل عملية إدخال'));
+    }
+
+    public function del_edi(Request $request)
+    {
       try {
-        $branch = Branch::find($data['id']);
-        if($branch){
-          $response = Gate::inspect('branches', $branch);
-          if($response->allowed())
+        $i = 0;
+        $arr = new ToArray();
+        $data = json_decode($request->pTableData, true);
+        $company = session('company');
+        $branches = $arr->to_array(Branch::query()->select('name')->where('company_id', $company)->get(), "name");
+        foreach($data as $data){
+
+          if(count($data) > 1)
           {
-            if(count($data) > 1)
-            {
-              $branch->update($data);
-              $status[$i] = $branch;
+            if($data['geolocation']??null){
+              $data['geolocation'] = explode(",", $data['geolocation']);
+              $data['latitude'] = $data['geolocation'][0];
+              $data['longitude'] = $data['geolocation'][1];
+            }
+            $validated = Validator::make($data,
+            ['id' => ['required','integer'],
+            'name' => ['string', 'max:50',Rule::notIn($branches) ],
+            'location' => ['string', 'max:50'],
+            'latitude' => [ 'numeric', 'between:-90,90'],
+            'longitude' => [ 'numeric', 'between:-180,180']]);
+            if ($validated->fails()) {
+              $status[$i] = $validated->errors();
               $i++;
+              $error = true;
+              continue;
+            }
+          }
+
+          $branch = Branch::find($data['id']);
+          if($branch){
+            $response = Gate::inspect('branches', $branch);
+            if($response->allowed())
+            {
+              if(count($data) > 1)
+              {
+                $branch->update($data);
+                $status[$i] = $branch;
+                $i++;
+              }
+              else
+              {
+                $branch->delete();
+                $status[$i] = $branch->id;
+                $i++;
+              }
             }
             else
             {
-              $branch->delete();
-              $status[$i] = $branch->id;
+              $status[$i] = $response->message();
               $i++;
             }
           }
-          else
-          {
-            $status[$i] = $response->message();
+          else {
+            $status[$i] = 'wrong id of branch';
             $i++;
           }
+
+        }}catch (\Exception $e) {
+          return response()->json(new Message($e->getMessage(), '100', false, 'error', 'error', 'خطأ'));
         }
-        else {
-          $status[$i] = 'wrong id of branch';
-          $i++;
-        }
-      }catch (\Exception $e) {
-        return response()->json(new Message($e->getMessage(), '100', false, 'error', 'error', 'خطأ'));
+        return response()->json(new Message($status, '200', isset($error)?false:true, 'info', "here status of every insertion", 'هذه حالة كل عملية إدخال'));
       }
     }
-    return response()->json(new Message($status, '200', isset($error)?false:true, 'info', "here status of every insertion", 'هذه حالة كل عملية إدخال'));
-  }
-}
